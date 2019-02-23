617462b76a5f189bfce8848f711e532f2f9187eb
Florian Weber <fwe@plutex.de>
2019-02-23 10:07:09 +0100

    change migration change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if (!User.find_by_name("admin"))
-  User.create!(name:"admin",email: 'admin@example.com', encrypted_password: 'adminadmin', admin:true, enabled:true)
+  User.create!(name:"admin",email: 'admin@example.com', password: 'adminadmin', admin:true, enabled:true)
 end
 
 def fake_name()
