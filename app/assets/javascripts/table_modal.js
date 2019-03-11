c24a9b1b5e3798ae13b29d00443b70fde84ac6e1
SimonIT <simonit.orig@gmail.com>
2019-03-11 12:01:29 +0100

    setting modal heading and buttons in footer

--- app/assets/javascripts/table_modal.js
+++ app/assets/javascripts/table_modal.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         "  <div class=\"modal-dialog\">" +
         "    <div class=\"modal-content\">" +
         "      <div class=\"modal-header\">" +
-        "        <h4 class=\"modal-title\">Modal Heading</h4>" +
+        "        <h4 class=\"modal-title\"></h4>" +
         "        <button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button>" +
         "      </div>" +
         "      <div class=\"modal-body\">" +
@@ -13,7 +13,6 @@ $(document).ready(function () {
         "        </table>" +
         "      </div>" +
         "      <div class=\"modal-footer\">" +
-        "        <button type=\"button\" class=\"btn btn-danger\" data-dismiss=\"modal\">Close</button>" +
         "      </div>" +
         "    </div>" +
         "  </div>" +
@@ -27,13 +26,27 @@ $(document).ready(function () {
 
     function openModal() {
         let tableModal = $("#tableModal").first();
-        let body = tableModal.find("table tbody").first();
+        let modalTitle = tableModal.find(".modal-title").first();
+        let modalBody = tableModal.find("table tbody").first();
+        let modalFooter = tableModal.find(".modal-footer").first();
         let values = $(this).children("td");
 
-        body.empty();
-        $(this).parents("table").first().find("thead tr th").each(function (i) {
-            body.append("<tr><th scope=\"row\">" + $(this).text() + "</th><td>" + $(values[i]).text() + "</td></tr>");
+        modalBody.empty();
+        modalFooter.empty();
+        let table = $(this).parents("table").first();
+        table.find("thead tr th").each(function (i) {
+            let head = $(this);
+            let type = head.attr("data-modal");
+            if (type !== "false") {
+                if (type === "button") {
+                    modalFooter.prepend($(values[i]).html());
+                } else {
+                    modalBody.append("<tr><th scope=\"row\">" + head.html() + "</th><td>" + $(values[i]).html() + "</td></tr>");
+                }
+            }
         });
+        modalTitle.text(table.attr("data-name"));
+        modalFooter.append("<button type=\"button\" class=\"btn btn-danger\" data-dismiss=\"modal\">Close</button>");
 
         tableModal.modal();
     }
