f0624a9601b5bcccfae34e4ceaf5345d1450d552
Falko Tschernay <falko20@schule.bremen.de>
2019-03-11 11:04:20 +0100

    8_admin_performance into staging (#20)
    * created controller for admin dashboard
    
    * added admin dashboard route
    
    * added sb-admin precompile
    
    * added meta
    
    * added sb-admin.js
    
    * added sb-admin.css
    
    * added admin dashboard
    
    * switched to fonts-awesome
    
    * rubocop
    
    * moved sidebar to layout
    
    * added performance controller routes and views
    
    * changed to datetime
    
    * edited form of performance

---
+++ app/controllers/admin/performances_controller.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+class Admin::PerformancesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @performances = Performance.all
+  end
+
+  def new
+    @performance = Performance.new
+  end
+
+  def create
+    @performance = Performance.new(performance_params)
+    if @performance.save
+      redirect_to(admin_performances_path)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @performance = Performance.find(params[:id])
+  end
+
+  def update
+    @performance = Performance.find(params[:id])
+    if @performance.update(performance_params)
+      flash[:success] = "performances was edited successful"
+      redirect_to(admin_performances_path)
+    else
+      flash[:danger] = "Something went wrong"
+      # redirect_to(edit_admin_performance_path(params[:id]))
+    end
+  end
+
+  def destroy
+    @performance = Performance.find(params[:id])
+    if @performance.destroy
+      flash[:success] = "performances was destroyed successful"
+      redirect_to admin_performances_path
+    else
+      flash[:danger] = "Something went wrong "
+    end
+  end
+
+  def show
+    @performance = Performance.find(params[:id])
+  end
+
+  private
+
+  begin
+    def performance_params
+      params.require(:performance).permit(:start,
+                                           :stop,
+                                           :prize,
+                                           :sell_allowed,
+                                           :stop_selling,
+                                           :number_of_tickets
+      )
+    end
+  end
+end
