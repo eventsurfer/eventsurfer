541268af45d7f14965f14bebffdbe6c000f75263
Florian Weber <florian.fynn.weber@gmail.com>
2019-04-13 21:51:09 +0200

    tmp

--- test/controllers/admin/access_to_areas_test.rb
+++ test/controllers/admin/access_to_areas_test.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
+
 require "test_helper"
 
 class AccessToAreasTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
-  test "get access to admin area without login" do
+  test "get access to admin area" do
     get admin_dashboards_path
     assert_redirected_to new_user_session_path
     sign_in create(:admin_user)
     get admin_dashboards_path
     assert_response :success
+  end
+
+  test "get access to area without permission" do
+    user = create(:admin_user2)
+    sign_in user
+    get admin
   end
 end
