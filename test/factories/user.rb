93b1ab8388c078b17ffebf4a134ddf12ded97586
Florian Weber <florian.fynn.weber@gmail.com>
2019-04-13 11:18:29 +0200

    setup test environment create factory

---
+++ test/factories/user.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    name {Faker::Name.name}
+    email {Faker::Internet.safe_email}
+    password {"testtest"}
+    enabled {true}
+    admin {false}
+    rank {0}
+    confirmed_at {DateTime.now}
+  end
+  factory :admin_user2, parent: :user do
+    admin {true}
+    rank {2}
+  end
+  factory :admin_user3, parent: :user do
+    admin {true}
+    rank {3}
+  end
+  factory :Admin_user, parent: :user do
+    admin {true}
+    rank {4}
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,10 +3,37 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "simplecov"
+
+SimpleCov.start do
+  add_filter "/test/"
+  add_filter "/config/"
+  add_filter "/vendor/"
+  add_group "Models", "app/models"
+  add_group "Controllers", "app/controllers"
+  add_group "Helpers", "app/helpers"
+end
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  include FactoryBot::Syntax::Methods
+  FactoryBot.find_definitions
 
   # Add more helper methods to be used by all tests here...
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
+end
+class ActionDispatch::IntegrationTest
+  def log_in_as(user, password: "testtest")
+    post new_user_session_path, params: {session: {email: user.email,
+                                                   password: password}}
+  end
 end
