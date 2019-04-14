992e9deec341ac5f37e47906874bbd1dddf62aea
Florian Weber <florian.fynn.weber@gmail.com>
2019-04-15 00:23:23 +0200

    new email layout

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,11 +4,19 @@ class OrderMailer < ApplicationMailer
   add_template_helper(ApplicationHelper)
   default from: "eventsurfer"
 
-  def entry_email(order, user)
+  def entry_order(order, user)
     @group_ticket = order
     @number_of_tickets = GroupTicket.where(order_id: order.id)
     @total_prize = "" # TODO: Change
     @user = user
     mail(subject: "EXAMPLE ENTRY ORDER", to: user.email)
+  end
+
+  def user_have_to_pay(order, user)
+    @group_ticket = order
+    @number_of_tickets = GroupTicket.where(order_id: order.id)
+    @total_prize = "" # TODO: Change
+    @user = user
+    @payment_info = "" # TODO: Change
   end
 end
