8f507555f4e555a1789607b3655d4ce501764e2c
Florian Weber <florian.fynn.weber@gmail.com>
2019-04-15 00:17:02 +0200

    change email content

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,10 +4,11 @@ class OrderMailer < ApplicationMailer
   add_template_helper(ApplicationHelper)
   default from: "eventsurfer"
 
-  def entry_email(order, user_email_address)
+  def entry_email(order, user)
     @group_ticket = order
     @number_of_tickets = GroupTicket.where(order_id: order.id)
     @total_prize = "" # TODO: Change
-    mail(subject: "EXAMPLE ENTRY ORDER", to: user_email_address)
+    @user = user
+    mail(subject: "EXAMPLE ENTRY ORDER", to: user.email)
   end
 end
