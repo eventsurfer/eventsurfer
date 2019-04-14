5e2ef3428015d208a3c458bce3def1a8272940ed
Florian Weber <florian.fynn.weber@gmail.com>
2019-04-15 00:10:56 +0200

    rubocop

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,4 +1,4 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 class OrderMailer < ApplicationMailer
   add_template_helper(ApplicationHelper)
@@ -8,6 +8,6 @@ class OrderMailer < ApplicationMailer
     @group_ticket = order
     @number_of_tickets = GroupTicket.where(order_id: order.id)
     @total_prize = "" # TODO: Change
-    mail(subject:"EXAMPLE ENTRY ORDER", to: user_email_address)
+    mail(subject: "EXAMPLE ENTRY ORDER", to: user_email_address)
   end
 end
