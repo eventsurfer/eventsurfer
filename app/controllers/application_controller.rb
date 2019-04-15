6346e8df7c08136203b9e9467289055aaf437f6a
SimonIT <simonit.orig@gmail.com>
2019-04-15 23:35:16 +0200

    extract locale from browser header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :is_admin?
 
+  def set_locale
+    I18n.locale = params[:locale] || extract_locale_from_header || I18n.default_locale
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
+
   def default_url_options
-    { locale: I18n.locale }
+    {locale: I18n.locale}
   end
 
   def is_admin?
@@ -19,10 +25,16 @@ class ApplicationController < ActionController::Base
   end
 
   protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  private
+
+  def extract_locale_from_header
+    request.env["HTTP_ACCEPT_LANGUAGE"].scan(/^[a-z]{2}/).first
+  end
 
 end
