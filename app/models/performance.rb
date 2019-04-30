fc4527ee8e8e9d68d3c8c7539a7c38912b530a5c
Falko <falko20@schule.bremen.de>
2019-05-01 00:34:33 +0200

    typo

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -4,7 +4,7 @@ class Performance < ApplicationRecord
   has_one :performance_location
   has_one :performance_event
   has_many :performance_tickets
-  has_many :carts, trough: :performance_carts
+  has_many :carts, through: :performance_carts
   has_one :event, through: :performance_event
   has_many :tickets, through: :performance_tickets
 
