ff023acef5e64e798941c33baf55462c150000d8
Florian Weber <florian.fynn.weber@gmail.com>
2019-05-01 19:47:09 +0200

    renamed and fixed tests create view for information about an ticket fixed attribute name for admin/ticket/index

---
+++ test/controllers/admin/tickets_controller_test.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "test_helper"
+class TicketsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  setup do
+    @admin_user = create(:admin_user)
+  end
+
+
+  test "get index" do
+    sign_in @admin_user
+    get admin_tickets_path
+    assert_response :success
+  end
+
+  test "get view of one ticket" do
+    sign_in @admin_user
+    ticket = create(:ticket)
+    get admin_ticket_path(ticket.id)
+    assert_redirected_to admin_tickets_path
+  end
+
+  test "remove ticket" do
+    sign_in @admin_user
+    ticket = create(:ticket)
+    delete
+  end
+end
