d20abd4a3c91b71f221a960cc9f99dde5d074870
Falko <falko20@schule.bremen.de>
2019-05-03 11:16:45 +0200

    fixed session based cart

--- app/controllers/frontend/carts_controller.rb
+++ app/controllers/frontend/carts_controller.rb
@@ -5,45 +5,52 @@
 class Frontend::CartsController < ApplicationController
   def add_item
     t = Ticket.find(params[:id])
-    if !@cart.include?(t.id)
-      @cart.push(t.id)
-    end
-
-    unless current_user.nil?
-      if PerformanceCart.find_by(performance_id: t.performance.id)
-        p t.performance.id
-        PerformanceCart.find_by(performance_id: t.performance.id).update(count: PerformanceCart.find_by(performance_id: (t.performance.id)).count + 1)
-      else
-        PerformanceCart.create(cart_id: Cart.find_by_user_id(current_user.id).id, performance_id: t.performance.id, count: 1)
+    exist = false
+    @cart.each do |element|
+      if element[0] == t.id
+        element[1] = element[1] + 1
+        exist = true
       end
     end
-    redirect_to(frontend_cart_path)
-  end
+    unless exist
+      @cart.push([t.id, 1])
+    end
 
-  def list_items
-    @items = []
-    unless current_user.nil?
-      PerformanceCart.where(cart_id: Cart.find_by_user_id(current_user.id)).each do |item|
-        @items.push(item)
-      end
+  unless current_user.nil?
+    if PerformanceCart.find_by(performance_id: t.performance.id)
+      p t.performance.id
+      PerformanceCart.find_by(performance_id: t.performance.id).update(count: PerformanceCart.find_by(performance_id: (t.performance.id)).count + 1)
     else
-      @cart.each do |id|
-        @items.push(Ticket.find(id.to_i))
-      end
+      PerformanceCart.create(cart_id: Cart.find_by_user_id(current_user.id).id, performance_id: t.performance.id, count: 1)
     end
   end
+  redirect_to(frontend_cart_path)
+end
 
-  def remove_item
-    @cart.delete(params[:id].to_i)
-    p @cart
-    redirect_to(frontend_cart_path)
-  end
-
-  def createOrder
-    this_order = Order.create(user_id: current_user.id)
+def list_items
+  @items = []
+  unless current_user.nil?
     PerformanceCart.where(cart_id: Cart.find_by_user_id(current_user.id)).each do |item|
-      GroupTicket.create(performance_id: item.performance_id, count: item.count, order_id: this_order.id)
+      @items.push([item.performance_id, item.count])
+    end
+  else
+    @cart.each do |item|
+      @items.push(item)
     end
   end
+end
+
+def remove_item
+  @cart.delete(params[:id].to_i)
+  p @cart
+  redirect_to(frontend_cart_path)
+end
+
+def createOrder
+  this_order = Order.create(user_id: current_user.id)
+  PerformanceCart.where(cart_id: Cart.find_by_user_id(current_user.id)).each do |item|
+    GroupTicket.create(performance_id: item.performance_id, count: item.count, order_id: this_order.id)
+  end
+end
 
 end
