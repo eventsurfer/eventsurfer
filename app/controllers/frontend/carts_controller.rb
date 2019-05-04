62668841d32209cf54c2714573356e6db837604e
Falko <falko20@schule.bremen.de>
2019-05-04 11:29:38 +0200

    rubocop

--- app/controllers/frontend/carts_controller.rb
+++ app/controllers/frontend/carts_controller.rb
@@ -16,41 +16,41 @@ class Frontend::CartsController < ApplicationController
       @cart.push([t.id, 1])
     end
 
-  unless current_user.nil?
-    if PerformanceCart.find_by(performance_id: t.performance.id)
-      p t.performance.id
-      PerformanceCart.find_by(performance_id: t.performance.id).update(count: PerformanceCart.find_by(performance_id: (t.performance.id)).count + 1)
+    unless current_user.nil?
+      if PerformanceCart.find_by(performance_id: t.performance.id)
+        p t.performance.id
+        PerformanceCart.find_by(performance_id: t.performance.id).update(count: PerformanceCart.find_by(performance_id: (t.performance.id)).count + 1)
+      else
+        PerformanceCart.create(cart_id: Cart.find_by_user_id(current_user.id).id, performance_id: t.performance.id, count: 1)
+      end
+    end
+    redirect_to(frontend_cart_path)
+  end
+
+  def list_items
+    @items = []
+    unless current_user.nil?
+      PerformanceCart.where(cart_id: Cart.find_by_user_id(current_user.id)).each do |item|
+        @items.push([item.performance_id, item.count])
+      end
     else
-      PerformanceCart.create(cart_id: Cart.find_by_user_id(current_user.id).id, performance_id: t.performance.id, count: 1)
+      @cart.each do |item|
+        @items.push(item)
+      end
     end
   end
-  redirect_to(frontend_cart_path)
-end
 
-def list_items
-  @items = []
-  unless current_user.nil?
+  def remove_item
+    @cart.delete(params[:id].to_i)
+    p @cart
+    redirect_to(frontend_cart_path)
+  end
+
+  def createOrder
+    this_order = Order.create(user_id: current_user.id)
     PerformanceCart.where(cart_id: Cart.find_by_user_id(current_user.id)).each do |item|
-      @items.push([item.performance_id, item.count])
-    end
-  else
-    @cart.each do |item|
-      @items.push(item)
+      GroupTicket.create(performance_id: item.performance_id, count: item.count, order_id: this_order.id)
     end
   end
-end
-
-def remove_item
-  @cart.delete(params[:id].to_i)
-  p @cart
-  redirect_to(frontend_cart_path)
-end
-
-def createOrder
-  this_order = Order.create(user_id: current_user.id)
-  PerformanceCart.where(cart_id: Cart.find_by_user_id(current_user.id)).each do |item|
-    GroupTicket.create(performance_id: item.performance_id, count: item.count, order_id: this_order.id)
-  end
-end
 
 end
