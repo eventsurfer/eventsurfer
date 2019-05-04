87e159bee7e047fc744a385d61266207a9f5ae2d
Florian Weber <florian.fynn.weber@gmail.com>
2019-05-04 20:58:32 +0200

    add email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
       address:              'smtp.gmail.com',
       port:                 587,
       domain:               'example.com',
-      user_name:            '<username>',
+      user_name:            'trotteltest02@gmail.com',
       password:             '<password>',
       authentication:       'plain',
       enable_starttls_auto: true }
