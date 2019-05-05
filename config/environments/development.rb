9eba614ec3434b84df62d14448d81a57412ce7d9
Florian Weber <florian.fynn.weber@gmail.com>
2019-05-05 17:04:45 +0200

    add mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,14 +58,14 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'eventsurfer.online', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'eventsurfer.online' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address:              'mail.eventsurfer.online',
-      port:                 465,
-      domain:               'eventsurfer.com',
-      user_name:            'no-reply@eventsurfer.online',
-      password:             'MySickPw123_',
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'example.com',
+      user_name:            'eventsurfer004@gmail.com',
+      password:             '',
       authentication:       'plain',
       enable_starttls_auto: true }
 end
