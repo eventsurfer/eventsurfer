02d9b39cede7e4565b1b344fdc6003363e60bf07
florianfynnweber <28307774+florianfynnweber@users.noreply.github.com>
2019-05-05 20:04:01 +0200

    Backend order (#62)
    * tmp
    
    * fix seeds
    
    * change views
    
    * add view to an order
    all tables in backend got the same design
    change routes
    
    * rubocop
    
    * orders can set to paid

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+class Admin::OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :is_admin?
+  before_action :checkPermission!
+  layout "adminDash"
+
+  def index
+    @groupeTicket = GroupTicket.all
+    @orders = Order.all
+    @paid = Order.paid_grouped
+    @unpaid = Order.unpaid_grouped
+    @user = User.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @groupTickets = GroupTicket.where(order_id: @order.id)
+    @performances = Performance.all
+    @user = User.find(@order.user_id)
+  end
+
+  def destroy
+    @groupeTicket = GroupTicket.where(order_id: params[:id])
+    if @groupeTicket.destroy
+      flash[:success] = "Order was destroyed successfull"
+      redirect_to admin_orders_path
+    end
+  end
+
+  def setPaid
+    p current_user
+    @order = Order.find(params[:id])
+    @order.setPaid(current_user)
+    redirect_to admin_orders_path
+  end
+
+  private
+
+    begin
+      def api_client_params
+        params.require(:order).permit()
+      end
+
+      def checkPermission!
+        if current_user.rank >= 2
+
+        else
+          redirect_to admin_dashboards_path
+        end
+      end
+    end
+end
