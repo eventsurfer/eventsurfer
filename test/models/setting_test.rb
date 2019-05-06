0bf269141f2dc4a4529b2ab1bccc6d0b4a0d3e3d
Florian Weber <florian.fynn.weber@gmail.com>
2019-05-06 06:02:04 +0200

    some funny tests

--- test/models/setting_test.rb
+++ test/models/setting_test.rb
@@ -2,6 +2,19 @@
 
 require "test_helper"
 class SettingTest < ActiveSupport::TestCase
+  test "get mail addresses" do
+    Setting.create(key: "MAILTO", value: "example@example.com")
+    assert_equal "example@example.com", Setting.getMailAddresses[0]
+  end
 
+  test "get payment info" do
+    Setting.create(key: "PAYMENT_INFO", value: "some funny payment_infos")
+    assert_equal "somefunnypayment_infos", Setting.getPaymentInfo.first
+  end
+
+  test "empty settings" do
+    assert_equal "root@localhost", Setting.getMailAddresses
+    assert_equal "no configuration was set", Setting.getPaymentInfo
+  end
 
 end
