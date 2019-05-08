f10bdd2eee0441008a820d5b842d3511c2439f61
Florian Weber <fwe@plutex.de>
2019-05-08 10:27:11 +0200

    add email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "railsTest_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {host: 'eventsurfer.online'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'example.com',
+      user_name: 'eventsurfer004@gmail.com',
+      password: '',
+      authentication: 'plain',
+      enable_starttls_auto: true}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
