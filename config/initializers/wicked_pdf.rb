ce4dd982cbc3a6e64e3e9b4e9916a8438f593758
Florian Weber <florian.fynn.weber@gmail.com>
2019-05-09 05:59:58 +0200

    fix issue #81 layout

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -14,11 +14,11 @@ WickedPdf.config = {
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
   # on linux systems use
-  exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
   # on windows systems use
-  #exe_path: Rails.root.join("bin/wkhtmltox/bin","wkhtmltopdf.exe").to_s
+  exe_path: Rails.root.join("bin/wkhtmltox/bin","wkhtmltopdf.exe").to_s
 }
