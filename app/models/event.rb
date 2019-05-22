1b4eba945cf4b9b5482d41e9410b529ba4ab7edc
Florian Weber <fwe@plutex.de>
2019-05-22 17:54:07 +0200

    some validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,8 @@ class Event < ApplicationRecord
   has_many :performance_events
   has_many :performances, through: :performance_events
 
+  validate :validate_start
+  validate :validate_start_before_start
   def getLocations(performances)
     tmp_id = []
     performances.each { |per| tmp_id.push(per.id) }
@@ -11,4 +13,17 @@ class Event < ApplicationRecord
     tmp_id.each {|id| tmp_loc.push(PerformanceLocation.find(id))}
     return tmp_loc
   end
+
+  def validate_start
+    if start.blank?
+      errors.add(:start, "muss ein gültiges Datum mit Uhrzeit sein.")
+    end
+  end
+
+  def validate_start_before_start
+    if stop != nil && start > stop
+      errors.add(:start, "darf nicht nach Ende liegen.")
+    end
+  end
+
 end
--- app/models/performance.rb
+++ app/models/performance.rb
@@ -9,4 +9,26 @@ class Performance < ApplicationRecord
   has_many :tickets, through: :performance_tickets
 
   has_one :location, through: :performance_location
+
+  validate :validate_start_before_start
+  validate :validate_start
+  validate :validate_start_with_event
+
+  def validate_start
+    if start.blank?
+      errors.add(:start, "muss ein gültiges Datum mit Uhrzeit sein.")
+    end
+  end
+
+  def validate_start_before_start
+    if stop != nil && start > stop
+      errors.add(:start, "darf nicht nach Ende liegen.")
+    end
+  end
+
+  def validate_performanceDate_with_eventDate
+    unless event.start <= start && stop <= event.stop
+      errors.add(:start, "Performance datum außerhalb des eventzeitraums")
+    end
+  end
 end
